dede5bd3c6049a9233da42ec1ad80e98cf81321f
Andherson Costa <112807863+oAndin@users.noreply.github.com>
2023-08-01 07:43:01 -0300

    Update script.js

--- public/js/script.js
+++ public/js/script.js
@@ -1,48 +1,47 @@
-console.log("Hello World!")
+console.log("Hello World!");
 
 const apenasNumeros = /[0-9]/g;
-const apenasLetras = /[a-z]/ig;
-
-let numero = Math.floor(Math.random() * 100) + 1;
-console.log(numero);
-
+const apenasLetras = /[a-z]/gi;
 
 let tentativa;
 let erros;
+let chances = 10;
+let numero = Math.floor(Math.random() * 100) + 1;
+console.log(numero);
 palpite.onfocus = function () {
-    palpite.value = '';
-}
+    palpite.value = "";
+};
 palpite.onblur = function jogo() {
-    // for(let i = 0; i >= 10 ; i++ ) {
-    tentativa = palpite.value;
-    if (palpite.value != '') {
-        if (Number(tentativa) != numero) {
-            console.log("errou");
-            palpite.style.border = '2px solid var(--lightRed)';
-            if (erradas.innerHTML != '') {
-                erradas.innerHTML += `- ${palpite.value}`;
+    chances--;
+    if (chances > 1) {
+        tentativa = palpite.value;
+        if (palpite.value != "") {
+            if (Number(tentativa) != numero) {
+                console.log("errou");
+                palpite.style.border = "2px solid var(--lightRed)";
+                if (erradas.innerHTML != "") {
+                    erradas.innerHTML += `- ${palpite.value}`;
+                } else {
+                    erradas.innerHTML += ` ${palpite.value}`;
+                }
             }
-            else {
-                erradas.innerHTML += ` ${palpite.value}`;
+            if (Number(tentativa) == numero) {
+                console.log("acertou");
+                palpite.style.border = "2px solid var(--green)";
             }
-
         }
-        if (Number(tentativa) == numero) {
-            console.log("acertou");
-            palpite.style.border = '2px solid var(--green)';
+        if (palpite.value == "") {
+            palpite.style.border = "2px solid var(--lightRed)";
+            erradas.innerHTML = `O palpite não pode estar em branco!`;
+        }
+
+        if (palpite.value != apenasNumeros) {
+            console.log("Letras");
+            palpite.value = palpite.value.replaceAll(apenasLetras, "");
+            erradas.innerHTML = `O palpite não pode ser com letras`;
         }
     }
-    if (palpite.value == '') {
-        palpite.style.border = '2px solid var(--lightRed)';
-        erradas.innerHTML = `O palpite não pode estar em branco!`;
+    if (chances == 0) {
+        alert("Você perdeu");
     }
-
-    if (palpite.value != apenasNumeros) {
-        console.log("Letras");
-        palpite = palpite.value.replaceAll(apenasLetras, '')
-        palpite.innerHTML = `${palpite.value}`;
-
-        erradas.innerHTML = `O palpite não pode ser com letras`;
-    }
-}
-
+};
