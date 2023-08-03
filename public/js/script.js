d646f9ead2c3c8fb857b877f0035155eec08b08f
oAndin <00andherson@gmail.com>
2023-08-02 21:26:17 -0300

    Update files

--- public/js/script.js
+++ public/js/script.js
@@ -1,33 +1,39 @@
 console.log("Hello World!");
-// icone.setAttribute('name', 'moon')
 const apenasNumeros = /[0-9]/g;
 const apenasLetras = /[a-z]/gi;
 
 let numero = Math.floor(Math.random() * 100) + 1;
 console.log(numero);
 let chances = 10;
-
+console.log(chances);
 let tentativa;
 let erros = [];
-
+dica.style.visibility = "hidden";
 palpite.onfocus = function () {
     palpite.value = "";
 };
 
 palpite.onblur = function jogo() {
-    chances--;
-    if (chances > 1) {
+    if (palpite.value == "") {
+        textoAviso.innerHTML = "Você precisa tentar advinhar o número!"
+        fora.style.visibility = 'visible';
+        aviso.style.visibility = 'visible';
+    }
+    if (chances > 0) {
+        chances--;
         tentativa = palpite.value;
         if (palpite.value != "") {
             if (Number(tentativa) != numero) {
                 erros.push(tentativa);
                 palpite.style.border = "2px solid var(--lightRed)";
-
+                biaInGame.src= "./public/img/bia-pensando.png";
                 if ( Number(tentativa) > numero) {
-                    
+                    dica.style.visibility = "visible";
+                    dica.setAttribute('name', 'down-arrow-alt')
                 }
                 if ( Number(tentativa) < numero) {
-
+                    dica.style.visibility = "visible";
+                    dica.setAttribute('name', 'up-arrow-alt')
                 }
 
             }
@@ -37,15 +43,11 @@ palpite.onblur = function jogo() {
                 biaInGame.src= "./public/img/bia-correta.png"
             }
         }
-        if (palpite.value == "") {
-            textoAviso.innerHTML = "Você precisa tentar advinhar o número!"
-            fora.style.visibility = 'visible';
-            aviso.style.visibility = 'visible';
-        }
     }
 
-    if (chances == 0) {
+    if (chances === 0) {
         biaInGame.src= "./public/img/bia-errada.png"
+        dica.style.visibility = "hidden";
     }
     erradas.innerHTML = erros;
 };
